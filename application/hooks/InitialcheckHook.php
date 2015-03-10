caabb41454546d72dcc27abbd38817659b6860f4
Arpit Maheshwari <arpit@mrbananaapp.com>
2015-03-10 18:23:12 +0530

    hook file

--- application/hooks/InitialcheckHook.php
+++ application/hooks/InitialcheckHook.php
@@ -38,48 +38,4 @@
             date_default_timezone_set("Asia/Kolkata");
         }
 
-        public function checkIfProductMetaNotEmpty()
-        {
-//            $this->checkProductMetaKeywordsNotEmpty();
-//            $this->checkProductMetaDescriptionNotEmpty();
-        }
-
-        public function checkProductMetaKeywordsNotEmpty()
-        {
-            $model = new Common_model();
-            $custom_record = new Custom_model();
-            $whereCondArr = array(
-                "meta_keywords" => ""
-            );
-            $records = $custom_record->getAllProductsList("p.product_id,product_title,product_code,gc_name,pc_name,cc_name", $whereCondArr);
-
-            if (!empty($records))
-            {
-                foreach ($records as $key => $value)
-                {
-                    $str = $records[$key]["product_title"] . " " . $records[$key]["product_code"] . " " . $records[$key]["gc_name"] . " " . $records[$key]["pc_name"] . " " . $records[$key]["cc_name"] . " " . SITE_NAME;
-                    $str = str_replace(" ", ", ", $str);
-                    $model->updateData(TABLE_PRODUCTS, array("meta_keywords" => $str), array("product_id" => $records[$key]["product_id"]));
-                }
-            }
-        }
-
-        public function checkProductMetaDescriptionNotEmpty()
-        {
-            $model = new Common_model();
-            $whereCondArr = array(
-                "meta_description" => ""
-            );
-            $records = $model->fetchSelectedData("product_id, product_description", TABLE_PRODUCTS, $whereCondArr);
-
-            if (!empty($records))
-            {
-                foreach ($records as $key => $value)
-                {
-                    $desc = substr($value["product_description"], 0, 200);
-                    $model->updateData(TABLE_PRODUCTS, array("meta_description" => $desc), array("product_id" => $value["product_id"]));
-                }
-            }
-        }
-
     }
