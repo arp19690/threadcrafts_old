ed7081082ea25648b19d4b37cdcd04864c3a4575
Arpit Maheshwari <arpit@mrbananaapp.com>
2015-03-16 01:14:25 +0530

    product listing in admin panel, changes

--- application/modules/admin/views/products/products-list.php
+++ application/modules/admin/views/products/products-list.php
@@ -15,7 +15,7 @@
                 <!-- BEGIN EXAMPLE TABLE PORTLET-->
                 <div class="portlet box blue">
                     <div class="portlet-title">
-                        <h4><i class="icon-edit"></i>Products</h4>
+                        <h4><i class="icon-list"></i>Products</h4>
                     </div>
                     <div class="portlet-body">
                         <div class="clearfix">
@@ -29,9 +29,9 @@
                             <thead>
                                 <tr>
                                     <th>Title</th>
-                                    <th>Code</th>
                                     <th>Category</th>
-                                    <th>Cost Price</th>
+                                    <th>Seller's Margin</th>
+                                    <th>Selling Price</th>
                                     <th>Status</th>
                                     <th>Action</th>
                                 </tr>
@@ -44,16 +44,19 @@
                                         $product_id = $a_value["product_id"];
                                         $product_title = $a_value["product_title"];
                                         $product_code = $a_value["product_code"];
+                                        $product_seller_price = $a_value["product_seller_price"];
                                         $product_cost_price = $a_value["product_price"];
                                         $product_status = getProductStatusText($a_value["product_status"]);
 
-                                        $category = '<a href="' . base_url_seller('products/category?gc=' . urlencode($a_value["gc_name"])) . '">' . $a_value["gc_name"] . '</a> -> <a href="' . base_url_seller('products/category?pc=' . urlencode($a_value["pc_name"])) . '">' . $a_value["pc_name"] . '</a> -> <a href="' . base_url_seller('products/category?cc=' . urlencode($a_value["cc_name"])) . '">' . $a_value["cc_name"] . '</a>';
+                                        $category = '<a href="' . base_url_admin('products/category?gc=' . urlencode($a_value["gc_name"])) . '">' . $a_value["gc_name"] . '</a> -> <a href="' . base_url_admin('products/category?pc=' . urlencode($a_value["pc_name"])) . '">' . $a_value["pc_name"] . '</a> -> <a href="' . base_url_admin('products/category?cc=' . urlencode($a_value["cc_name"])) . '">' . $a_value["cc_name"] . '</a>';
                                         ?>
                                         <tr>
-                                            <td><?php echo $product_title; ?></td>
-                                            <td><?php echo $product_code; ?></td>
+                                            <td>
+                                                <p><?php echo $product_title; ?> (<strong><?php echo $product_code; ?></strong>)</p>
+                                            </td>
                                             <td><?php echo $category; ?></td>
-                                            <td><?php echo DEFAULT_CURRENCY_SYMBOL . number_format($product_cost_price,2); ?></td>
+                                            <td><?php echo DEFAULT_CURRENCY_SYMBOL . number_format($product_seller_price, 2); ?></td>
+                                            <td><?php echo DEFAULT_CURRENCY_SYMBOL . number_format($product_cost_price, 2); ?></td>
                                             <td class="center"><?php echo $product_status; ?></td>
                                             <td class="center">
                                                 <a href="<?php echo base_url("admin/products/productDetail/" . $product_id); ?>" title="View Detail"><i class="icon-search"></i>&nbsp;Details</a><br/>
