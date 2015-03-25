857b59b98faa1b35f0d198c42564d40ec70d78e2
Arpit Maheshwari <arpit@mrbananaapp.com>
2015-03-25 16:26:48 +0530

    to add new seller and add bank details

--- application/modules/admin/controllers/sellers.php
+++ application/modules/admin/controllers/sellers.php
@@ -22,7 +22,125 @@
 
         public function addSeller($seller_id = NULL)
         {
-            
+            $model = new Common_model();
+
+            if ($this->input->post())
+            {
+                $arr = $this->input->post();
+
+                $is_email_exists = $this->checkIfSellerEmailExists(strtolower($arr['seller_email']), $seller_id);
+                if (!$is_email_exists)
+                {
+                    $data_array = array(
+                        'seller_fullname' => addslashes(ucwords($arr['seller_fullname'])),
+                        'seller_company_name' => addslashes(ucwords($arr['seller_company_name'])),
+                        'seller_company_regid' => addslashes($arr['seller_company_regid']),
+                        'seller_email' => (strtolower($arr['seller_email'])),
+                        'seller_address_line1' => addslashes($arr['seller_address_line1']),
+                        'seller_address_line2' => addslashes($arr['seller_address_line2']),
+                        'seller_location' => addslashes($arr['seller_location']),
+                        'seller_postcode' => ($arr['seller_postcode']),
+                        'seller_mobile' => ($arr['seller_mobile']),
+                        'seller_internal_comments' => addslashes($arr['seller_internal_comments']),
+                        'seller_ipaddress' => USER_IP,
+                        'seller_useragent' => USER_AGENT
+                    );
+
+                    if ($seller_id == NULL)
+                    {
+                        // insert
+                        $newpassword = getRandomNumberLength(time(), 6);
+                        $data_array['seller_password'] = md5($newpassword);
+                        $data_array['seller_joined_date'] = date('Y-m-d H:i:s');
+                        $model->insertData(TABLE_SELLER, $data_array);
+                        $seller_id = $this->db->insert_id();
+                        $this->session->set_flashdata("success", "Seller details added");
+                        redirect(base_url_admin('sellers/addSellerBank/' . $seller_id));
+                    }
+                    else
+                    {
+                        // update
+                        $model->updateData(TABLE_SELLER, $data_array, array('seller_id' => $seller_id));
+                        $this->session->set_flashdata("success", "Seller details updated");
+                        redirect(base_url_admin('sellers/sellerDetail/' . $seller_id));
+                    }
+                }
+                else
+                {
+                    $this->session->set_flashdata("error", "Email already exists");
+                    $this->session->set_flashdata("post", $arr);
+                    redirect(base_url_admin('sellers/addSeller'));
+                }
+            }
+            else
+            {
+                if ($seller_id == NULL)
+                {
+                    $data["form_heading"] = "Add Seller";
+                }
+                else
+                {
+                    $data["form_heading"] = "Edit Seller";
+                }
+
+                $this->template->write_view("content", "sellers/seller-form", $data);
+                $this->template->render();
+            }
+        }
+
+        public function addSellerBank($seller_id, $sb_id = NULL)
+        {
+            $model = new Common_model();
+
+            if ($this->input->post())
+            {
+                $arr = $this->input->post();
+
+                $data_array = array(
+                    'sb_seller_id' => $seller_id,
+                    'sb_bank_name' => addslashes($arr['sb_bank_name']),
+                    'sb_account_holder' => addslashes($arr['sb_account_holder']),
+                    'sb_account_number' => ($arr['sb_account_number']),
+                    'sb_status' => '1',
+                    'sb_ifsc_code' => strtoupper($arr['sb_ifsc_code']),
+                    'sb_ipaddress' => USER_IP,
+                    'sb_useragent' => USER_AGENT
+                );
+
+                // to deactivate all other bank details
+                $model->updateData(TABLE_SELLER_BANK, array('sb_status' => '0'), array('sb_id' => $sb_id));
+                if ($sb_id == NULL)
+                {
+                    // to insert new bank details
+                    $model->insertData(TABLE_SELLER_BANK, $data_array);
+                    $this->session->set_flashdata("success", "Bank details added");
+                    redirect(base_url_admin('sellers/addSellerDocuments/' . $seller_id));
+                }
+                else
+                {
+                    // to update the current bank details and activate it
+                    $model->updateData(TABLE_SELLER_BANK, $data_array, array('sb_id' => $sb_id));
+                    $this->session->set_flashdata("success", "Bank details updated");
+                    redirect(base_url_admin('sellers/sellerDetail/' . $seller_id));
+                }
+            }
+            else
+            {
+                if ($seller_id == NULL)
+                {
+                    $data["form_heading"] = "Add Bank Details";
+                }
+                else
+                {
+                    $data["form_heading"] = "Edit Bank Details";
+                    $bank_record = $model->fetchSelectedData('*', TABLE_SELLER_BANK, array('sb_seller_id' => $seller_id), 'sb_id', 'DESC', 1);
+                    $data['bank_record'] = @$bank_record[0];
+                    $data["form_action"] = base_url_admin('sellers/addSellerBank/' . $seller_id . '/' . @$bank_record[0]['sb_id']);
+                }
+
+                $this->template->write_view("content", "sellers/seller-bank-form", $data);
+                $this->template->render();
+            }
         }
 
         public function deactivateSeller($seller_id)
@@ -116,7 +234,7 @@
                     $this->session->set_flashdata("error", "An error occured while uploading image");
                 }
 
-                redirect(base_url_admin('seller/sellerDetail/' . $seller_id));
+                redirect(base_url_admin('sellers/sellerDetail/' . $seller_id));
             }
         }
 
@@ -135,7 +253,7 @@
                     $this->session->set_flashdata("error", "An error occured while uploading logo");
                 }
 
-                redirect(base_url_admin('seller/sellerDetail/' . $seller_id));
+                redirect(base_url_admin('sellers/sellerDetail/' . $seller_id));
             }
         }
 
--- application/modules/admin/views/sellers/seller-form.php
+++ application/modules/admin/views/sellers/seller-form.php
@@ -1,5 +1,11 @@
 <?php
     $result = array();
+
+    if ($this->session->flashdata('post'))
+    {
+        $record = $this->session->flashdata('post');
+    }
+
     if (isset($record))
     {
         foreach ($record as $key => $value)
@@ -9,16 +15,21 @@
     }
     else
     {
-        $result["user_id"] = "";
-        $result["first_name"] = "";
-        $result["last_name"] = "";
-        $result["user_email"] = "";
-        $result["user_address"] = "";
-        $result["user_location"] = "";
-        $result["user_contact"] = "";
-        $result["user_dob"] = "";
-        $result["user_status"] = "";
-        $result["user_postcode"] = "";
+        $result["seller_id"] = "";
+        $result["seller_fullname"] = "";
+        $result["seller_company_name"] = "";
+        $result["seller_company_regid"] = "";
+        $result["seller_email"] = "";
+        $result["seller_address_line1"] = "";
+        $result["seller_address_line2"] = "";
+        $result["seller_location"] = "";
+        $result["seller_mobile"] = "";
+        $result["seller_postcode"] = "";
+        $result["seller_internal_comments"] = "";
+        $result["sb_bank_name"] = "";
+        $result["sb_account_holder"] = "";
+        $result["sb_account_number"] = "";
+        $result["sb_ifsc_code"] = "";
     }
 
     if (!isset($form_action))
@@ -56,70 +67,70 @@
                     <div class="portlet-body form">
                         <!-- BEGIN FORM-->
                         <form action="<?php echo $form_action; ?>" method="post" class="form-horizontal">
-                            <input type="hidden" name="user_id" value="<?php echo set_value("user_id", $result["user_id"]); ?>"/>
                             <div class="control-group">
-                                <label class="control-label">First Name<span class="required">*</span></label>
+                                <label class="control-label">Owner Name<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input type="text" name="first_name" required="required" value="<?php echo set_value("first_name", $result["first_name"]); ?>" data-required="1" class="span6 m-wrap"/>
+                                    <input type="text" name="seller_fullname" required="required" value="<?php echo set_value("seller_fullname", $result["seller_fullname"]); ?>" data-required="1" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">Last Name<span class="required">*</span></label>
+                                <label class="control-label">Company Name<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="last_name" required="required" value="<?php echo set_value("last_name", $result["last_name"]); ?>" type="text" class="span6 m-wrap"/>
+                                    <input type="text" name="seller_company_name" required="required" value="<?php echo set_value("seller_company_name", $result["seller_company_name"]); ?>" data-required="1" class="span6 m-wrap"/>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Company Reg. Number<span class="required">*</span></label>
+                                <div class="controls">
+                                    <input type="text" name="seller_company_regid" required="required" value="<?php echo set_value("seller_company_regid", $result["seller_company_regid"]); ?>" data-required="1" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label">Email Address<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="user_email" required="required" value="<?php echo set_value("user_email", $result["user_email"]); ?>" type="text" class="span6 m-wrap"/>
+                                    <input name="seller_email" required="required" value="<?php echo set_value("seller_email", $result["seller_email"]); ?>" type="text" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">Password</label>
+                                <label class="control-label">Mobile Number<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="user_password" value="<?php echo set_value("user_password"); ?>" type="password" class="span6 m-wrap"/>
+                                    <div class="input-prepend">
+                                        <span class="add-on">+91</span>
+                                        <input name="seller_mobile" required="required" maxlength="10" value="<?php echo set_value("seller_mobile", $result["seller_mobile"]); ?>" type="text" class="m-wrap"/>
+                                    </div>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">Contact Number<span class="required">*</span></label>
+                                <label class="control-label">Address Line 1<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="user_contact" required="required" maxlength="15" value="<?php echo set_value("user_contact", $result["user_contact"]); ?>" type="text" class="span6 m-wrap"/>
+                                    <input name="seller_address_line1" required="required" value="<?php echo set_value("seller_address_line1", $result["seller_address_line1"]); ?>" type="text" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">Address<span class="required">*</span></label>
+                                <label class="control-label">Address Line 2</label>
                                 <div class="controls">
-                                    <textarea name="user_address" required="required" rows="3" class="span6 m-wrap"><?php echo set_value("user_address", $result["user_address"]); ?></textarea>
+                                    <input name="seller_address_line1" value="<?php echo set_value("seller_address_line1", $result["seller_address_line1"]); ?>" type="text" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label">Location<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="user_location" required="required" value="<?php echo set_value("user_location", $result["user_location"]); ?>" type="text" class="span6 m-wrap gMapLocation"/>
+                                    <input name="seller_location" required="required" value="<?php echo set_value("seller_location", $result["seller_location"]); ?>" type="text" class="span6 m-wrap gMapLocation"/>
                                 </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label">Postcode<span class="required">*</span></label>
                                 <div class="controls">
-                                    <input name="user_postcode" required="required" value="<?php echo set_value("user_postcode", $result["user_postcode"]); ?>" type="text" class="span6 m-wrap"/>
+                                    <input name="seller_postcode" required="required" value="<?php echo set_value("seller_postcode", $result["seller_postcode"]); ?>" type="text" class="span6 m-wrap"/>
                                 </div>
                             </div>
                             <div class="control-group">
-                                <label class="control-label">Date of Birth<span class="required">*</span></label>
+                                <label class="control-label">Internal Comments</label>
                                 <div class="controls">
-                                    <input name="user_dob" required="required" value="<?php echo set_value("user_dob", $result["user_dob"]); ?>" type="text" class="span6 m-wrap datepicker"/>
+                                    <textarea name="seller_internal_comments" value="<?php echo set_value("seller_internal_comments", $result["seller_internal_comments"]); ?>" class="span6 m-wrap"></textarea>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">User Status<span class="required">*</span></label>
-                                <div class="controls">
-                                    <select name="user_status" class="span6 m-wrap" required="required">
-                                        <option value="1" <?php echo set_select("user_status", "1", $result["user_status"] == "1") ?>>Active</option>
-                                        <option value="0" <?php echo set_select("user_status", "0", $result["user_status"] == "0") ?>>Deactive</option>
-                                    </select>
-                                </div>
-                            </div>
+
                             <div class="form-actions">
                                 <button type="submit" class="btn green">Submit</button>
                             </div>
