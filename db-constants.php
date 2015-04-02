91bad2ecf2c6578f89d8296b25d2abf950a71594
Arpit Maheshwari <arpit@mrbananaapp.com>
2015-04-02 17:15:27 +0530

    added user address db table constant

--- db-constants.php
+++ db-constants.php
@@ -3,6 +3,7 @@
     define("TABLE_ADMIN", "admin");
     define("TABLE_SELLER", "sellers");
     define("TABLE_USERS", "users");
+    define("TABLE_USER_ADDRESSES", "user_address");
     define("TABLE_ADMIN_LOG", "admin_log");
     define("TABLE_SELLER_LOG", "seller_log");
     define("TABLE_USER_LOG", "user_log");
