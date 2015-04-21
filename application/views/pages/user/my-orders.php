3dc25ec4e4fe858f37d76da2bf0be612a1aa701f
Arpit Maheshwari <arpit@mrbananaapp.com>
2015-04-21 15:32:09 +0530

    minor changes in the my-orders page

--- application/views/pages/user/my-orders.php
+++ application/views/pages/user/my-orders.php
@@ -6,29 +6,6 @@
             <!--  = Sidebar =  -->
             <!--  ==========  -->
             <aside class="span3">
-                <div class="sidebar-item">
-
-                    <!--  ==========  -->
-                    <!--  = Sidebar Title =  -->
-                    <!--  ==========  -->
-                    <div  class="underlined">
-                        <h3><span class="light">My</span> Orders</h3>
-                    </div>
-
-                    <!--  ==========  -->
-                    <!--  = Menu (affix) =  -->
-                    <!--  ==========  -->
-                    <div class="row">
-                        <div class="span3" id="spyMenu">
-                            <div class="widget">
-                                <ul class="nav nav-pills nav-stacked my-account-sidebar">
-                                    <li class="active"><a href="#orders">Orders <i class="icon-caret-right pull-right"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
             </aside> <!-- /sidebar -->
 
             <!--  ==========  -->
@@ -42,8 +19,8 @@
                             <tr>
                                 <th>Order ID</th>
                                 <th>Shipping Details</th>
+                                <th>Price</th>
                                 <th>Status</th>
-                                <th style="text-align: right;">Price</th>
                                 <th style="text-align: center">Details</th>
                             </tr>
                         </thead>
@@ -57,9 +34,9 @@
                                         <tr>
                                             <td class="desc">#<?php echo stripslashes($value['sd_order_id']); ?></td>
                                             <td class=""><?php echo stripslashes(trim($value['sd_shipping_address'] . ' ' . $value['sd_shipping_location'] . ' ' . $value['sd_shipping_postcode'])); ?></td>
+                                            <td class=""><?php echo DEFAULT_CURRENCY_SYMBOL . number_format($value['payment_amount'], 2); ?></td>
                                             <td class=""><?php echo getOrderStatusText($value['sod_order_status']); ?></td>
-                                            <td class="price"><?php echo DEFAULT_CURRENCY_SYMBOL . number_format($value['payment_amount'], 2); ?></td>
-                                            <td class="desc" style="text-align: center">
+                                            <td class="" style="text-align: center">
                                                 <a href="#" title="View order details" class="btn btn-success"><span class="icon icon-search"></span>&nbsp;Details</a>
                                             </td>
                                         </tr>
