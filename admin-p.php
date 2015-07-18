131bf9fac868f206754dab63d77e370d537672a9
Arpit Maheshwari <arp19690@gmail.com>
2015-07-18 12:11:11 +0530

    testing

--- admin-p.php
+++ admin-p.php
@@ -1,19 +1,21 @@
 <?php
-/***************************************************************************
-*                                                                          *
-*   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
-*                                                                          *
-* This  is  commercial  software,  only  users  who have purchased a valid *
-* license  and  accept  to the terms of the  License Agreement can install *
-* and use this program.                                                    *
-*                                                                          *
-****************************************************************************
-* PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
-* "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
-****************************************************************************/
+
+/* * *************************************************************************
+ *                                                                          *
+ *   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
+ *                                                                          *
+ * This  is  commercial  software,  only  users  who have purchased a valid *
+ * license  and  accept  to the terms of the  License Agreement can install *
+ * and use this program.                                                    *
+ *                                                                          *
+ * ***************************************************************************
+ * PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
+ * "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
+ * ************************************************************************** */
 
 $php_value = phpversion();
-if (version_compare($php_value, '5.3.0') == -1) {
+if (version_compare($php_value, '5.3.0') == -1)
+{
     echo 'Currently installed PHP version (' . $php_value . ') is not supported. Minimal required PHP version is  5.3.0.';
     die();
 }
@@ -21,10 +23,12 @@ if (version_compare($php_value, '5.3.0') == -1) {
 define('AREA', 'A');
 define('ACCOUNT_TYPE', 'admin');
 
-try {
+try
+{
     require(dirname(__FILE__) . '/init.php');
 
     fn_dispatch();
-} catch (Tygh\Exceptions\AException $e) {
+} catch (Tygh\Exceptions\AException $e)
+{
     $e->output();
 }
